07e501bb4670422819795537d845754a6d58775b
mainagerald <flavianmaina2401@gmail.com>
2024-03-18 21:02:43 +0300

    circuit breaker

--- order-service/src/main/java/com/myServices/orderService/controller/OrderController.java
+++ order-service/src/main/java/com/myServices/orderService/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.myServices.orderService.controller;
 
 import com.myServices.orderService.dto.OrderRequest;
 import com.myServices.orderService.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +15,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallback")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order placed successfully!";
+    }
+
+    public String fallback(OrderRequest orderRequest, RuntimeException runtimeException)
+    {
+        return "Uh oh! Something went wrong. Try again shortly.";
     }
 }
